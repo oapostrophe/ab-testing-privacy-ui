b046bec3fd909ff62193d04a7991368c612b126c
SO <swoconnor2@gmail.com>
2020-08-06 17:05:46 -0700

    Initial commit with experiment 2 implementation

--- example_site/static/js/main.js
+++ example_site/static/js/main.js
@@ -2,89 +2,110 @@ function disableScroll() {
   // Get the current page scroll position 
   scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
   scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
-
+ 
   // if any scroll is attempted, set this to the previous value 
   window.onscroll = function() { 
     window.scrollTo(scrollLeft, scrollTop); 
     }; 
 } 
-
+ 
 function enableScroll() { 
   window.onscroll = function() {}; 
 } 
-
+ 
 function determine_layout() {
   var x = document.getElementById("notification");
   var overlay=document.getElementById("myNav");
+  var button1 = document.getElementById("button1");
   var button2 = document.getElementById("button2");
   var screenWidthString=String(window.innerWidth+100)+"px";
   var screenHeight=String(window.innerHeight -340)+"px";
   var bannerScreenHeight=String(window.innerHeight-200)+"px";
   var middleScreenHeight=String((window.innerHeight)/3)+"px";
   var acceptCookies = getCookie("acceptCookies")
+  var form= document.getElementById("form");
+  var toggle=document.getElementById("toggle");
+  var one_button=document.getElementById("one_button");
+  ///form.style.display="none";
   x.style.display="block";
-  if (String(acceptCookies)=="1"){
-    x.style.display="none";
-  }
+ 
+  ///if (String(acceptCookies)=="1"){
+    ///x.style.display="none";
+  /// }
+  desktopLayout=5;
   if (mobile == false){
+    x.style.bottom=screenHeight;
+    x.style.left = "";
+    x.style.right="10px"
     if (String(desktopLayout) == "0"){
-      x.style.bottom="10px";
-      x.style.left = "10px";
-      x.style.right="";
+      
     }
-    if (String(desktopLayout) == "1"){
-      x.style.bottom="10px";
-      x.style.left = "24px";
-      x.style.right="24px";
-      x.style.maxWidth=screenWidthString;
+    else if (String(desktopLayout) == "1"){
+      button2.style.fontWeight="normal";
+      button2.style.background="skyblue";
+      button2.style.border="1px solid black"
+      button1.style.background="darkblue";
+      button1.style.border="1px solid white"
+      button1.style.color="white";
     }
-    if (String(desktopLayout) == "2"){
-      x.style.bottom="10px";
-      x.style.left = "";
-      x.style.right="10px"
+    else if (String(desktopLayout) == "2"){
+      button2.setAttribute("onClick", "javascript: myForm()");
     }
-    if (String(desktopLayout) == "3"){
-      x.style.bottom=screenHeight;
-      x.style.left = "10px";
-      x.style.right="";
+    else if (String(desktopLayout) == "3"){
+      button2.setAttribute("onClick", "javascript: myToggle()");
     }
-    if (String(desktopLayout) == "4"){
-      x.style.bottom=bannerScreenHeight;
-      x.style.left = "24px";
-      x.style.right="24px";
-      x.style.maxWidth=screenWidthString;
-    }
-    if (String(desktopLayout) == "5"){
-      x.style.bottom=screenHeight;
-      x.style.left = "";
-      x.style.right="10px"
-    }
-    if (String(desktopLayout) == "6"){
-      x.style.bottom=middleScreenHeight;
-      x.style.left = "24px";
-      x.style.right="24px";
-    }
-    if (String(desktopLayout)=="7"){
+    else if (String(desktopLayout) == "4"){
       x.style.display="none";
+      var screenHeight=String(window.innerHeight -530)+"px";
+      toggle.style.bottom=screenHeight;
+      toggle.style.left = "";
+      toggle.style.right="24px";
+    }
+    else if (String(desktopLayout) == "5"){
+      x.style.display="none";
+      var screenHeight=String(window.innerHeight -530)+"px";
+      one_button.style.bottom=screenHeight;
+      one_button.style.left="";
+      one_button.style.right="10px";
     }
   }
   else{
+    x.style.bottom=middleScreenHeight
     x.style.left = "24px";
     x.style.right="24px";
     if (String(mobileLayout)=="0"){
-      x.style.bottom="10px"
+      
     }
-    if (String(mobileLayout)=="1"){
-      x.style.bottom=middleScreenHeight;
+    else if (String(mobileLayout)=="1"){
+      button2.style.fontWeight="normal";
+      button2.style.background="skyblue";
+      button2.style.border="1px solid black"
+      button1.style.background="darkblue";
+      button1.style.border="1px solid white"
+      button1.style.color="white";
     }
-    if (String(mobileLayout)=="2"){
-      x.style.bottom=screenHeight;
+    else if (String(mobileLayout)=="2"){
+      button2.setAttribute("onClick", "javascript: myForm()");
     }
-    if (String(mobileLayout)=="3"){
+    else if (String(mobileLayout)=="3"){
+      button2.setAttribute("onClick", "javascript: myToggle()");
+    }
+    else if (String(mobileLayout)=="4"){
       x.style.display="none";
+      var screenHeight=String(window.innerHeight -530)+"px";
+      toggle.style.bottom=screenHeight;
+      toggle.style.left = "";
+      toggle.style.right="24px";
+    }
+    else if (String(mobileLayout)=="5"){
+      x.style.display="none";
+      one_button.style.bottom=screenHeight;
+      one_button.style.left="";
+      one_button.style.right="10px";
     }
   }
 }
+ 
 function linkClicked(){
   var x = document.getElementById("notification");
   var middleScreenHeight=String((window.innerHeight)/3)+"px"
@@ -94,27 +115,99 @@ function linkClicked(){
   x.style.right="24px";
   x.style.maxWidth="400px";
 }
-
+ 
 function endOverlay(){
   var overlay=document.getElementById("myNav");
   overlay.style.display="none"
 }
 function readMore(){
+  var link_accept=document.getElementById("link_accept")
+  var button0=document.getElementById("button0");
+  var button1=document.getElementById("button1");
   var button2 = document.getElementById("button2");
+  var options= document.getElementById("options");
   var text = document.getElementById("text");
   text.innerHTML="Under the California Consumer Privacy Act, you have the right to opt-out of the sale of your personal information to third parties. You may exercise your right to opt out of the sale of personal information by clicking the 'Deny Cookies' button.";
-  button2.innerHTML="Don't allow";
-  button2.setAttribute( "onClick", "javascript: myFunction(), enableScroll(), endOverlay();" )
+  button1.style.display="none";
+  button2.style.display="none";
+  options.style.display="block";
+  button0.style.display="block";
+  link_accept.style.display="none";
+  ///button2.innerHTML="Don't allow";
+  ///button2.setAttribute( "onClick", "javascript: myFunction(), enableScroll(), endOverlay();" )
+}
+function myClose() {
+  var x=document.getElementById("notification");
+  var overlay=document.getElementById("myNav");
+  var form=document.getElementById("form");
+  var toggle=document.getElementById("toggle");
+  var one_button=document.getElementById("one_button");
+  x.style.display="none";
+  form.style.display="none";
+  toggle.style.display="none";
+  one_button.style.display="none";
+  overlay.style.display="none";
 }
 function myFunction() {
-  document.cookie="acceptCookies=1; path=/; ";
+  ///document.cookie="acceptCookies=1; path=/; ";
+  var toggle=document.getElementById("toggle");
+  var form=document.getElementById("form");
   var x = document.getElementById("notification");
+  toggle.style.display="none";
+  form.style.display="none";
   if (x.style.display === "none") {
     x.style.display = "block";
   } 
   else {
     x.style.display = "none";
   }
+}
+ 
+function myForm(){
+  var x= document.getElementById("notification");
+  var overlay=document.getElementById("myNav")
+  overlay.style.display="block";
+  x.style.display="none";
+  var middleScreenHeight=String((window.innerHeight)/3-50)+"px";
+  var screenHeight=String(window.innerHeight -530)+"px";
+  var form=document.getElementById("form");
+  form.style.bottom=middleScreenHeight;
+  form.style.left = "10px";
+  form.style.right="10px";
+}
+function myToggle(){
+  var x = document.getElementById("notification");
+  var overlay=document.getElementById("myNav")
+  overlay.style.display="block";
+  x.style.display="none";
+  var screenHeight=String(window.innerHeight -530)+"px";
+  var middleScreenHeight=String((window.innerHeight)/3)+"px";
+  var toggle=document.getElementById("toggle");
+  toggle.style.bottom=middleScreenHeight;
+  toggle.style.left = "24px";
+  toggle.style.right="24px";
+}
+function myDirectToggle(){
+  var x = document.getElementById("notification");
+  x.style.display="none";
+  var screenHeight=String(window.innerHeight -530)+"px";
+  var middleScreenHeight=String((window.innerHeight)/3)+"px";
+  var toggle=document.getElementById("toggle");
+  toggle.style.bottom=screenHeight;
+  toggle.style.left = "";
+  toggle.style.right="24px";
+}
+ 
+ 
+function one_button(){
+  var x=document.getElementById("notification")
+  var screenHeight=String(window.innerHeight -350)+"px";
+  var middleScreenHeight=String((window.innerHeight)/3)+"px";
+  var one_button=document.getElementById("one_button");
+  x.style.display="none";
+  one_button.style.bottom=screenHeight;
+  one_button.style.left="";
+  one_button.style.right="10px";
 }
     
 function getCookie(cname) {
@@ -132,3 +225,6 @@ function getCookie(cname) {
   }
   return "";
 }
+ 
+ 
+
